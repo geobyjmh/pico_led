ab308aa2d725d33818d2df6af906fa97b5568b97
geobyjmh <38050735+geobyjmh@users.noreply.github.com>
2022-02-18 10:04:25 +0000

    refactor rgb code

--- pico_led/NeoPixel.py
+++ pico_led/NeoPixel.py
@@ -16,13 +16,17 @@ class NeoPixel(object):
     def get_number_of_leds(self):
         return ws2812_pio_driver.get_number_of_leds()
     
+    def __rgb(self, c):
+        r = int(((c >> 8) & 0xFF) * self.brightness)
+        g = int(((c >> 16) & 0xFF) * self.brightness)
+        b = int((c & 0xFF) * self.brightness)
+        combined_rgb = (g<<16) + (r<<8) + b
+        return combined_rgb
+        
     def pixels_show(self):
         dimmer_ar = array.array("I", [0 for _ in range(self.get_number_of_leds())])
         for i,c in enumerate(self.ar):
-            r = int(((c >> 8) & 0xFF) * self.brightness)
-            g = int(((c >> 16) & 0xFF) * self.brightness)
-            b = int((c & 0xFF) * self.brightness)
-            dimmer_ar[i] = (g<<16) + (r<<8) + b
+            dimmer_ar[i] = self.__rgb(c)
         ws2812_pio_driver.write_to_neopixel(dimmer_ar)
 
     def pixels_set(self,color, i):
