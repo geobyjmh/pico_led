c46764a22bdb4e88f377f6a58ffc8f30cc7067e3
geobyjmh <38050735+geobyjmh@users.noreply.github.com>
2022-02-18 09:54:05 +0000

    Refactor rainbow pattern

--- pico_led/NeoPixel.py
+++ pico_led/NeoPixel.py
@@ -25,38 +25,10 @@ class NeoPixel(object):
             dimmer_ar[i] = (g<<16) + (r<<8) + b
         ws2812_pio_driver.write_to_neopixel(dimmer_ar)
 
-    def pixels_set(self, i, color):
+    def pixels_set(self,color, i):
         self.ar[i] = (color[1]<<16) + (color[0]<<8) + color[2]
 
     def pixels_fill(self, color):
         for i in range(len(self.ar)):
-            self.pixels_set(i, color)
-
-    def color_chase(self, color, length):
-        #for i in range(self.get_number_of_leds()):
-        self.pixels_set(length, color)
-            #time.sleep(wait)
-#         self.pixels_show()
-#         time.sleep(0.2)
+            self.pixels_set(color, i)
      
-    def wheel(self, pos):
-        # Input a value 0 to 31 to get a color value.
-        # The colours are a transition r - g - b - back to r.
-        if pos < 0 or pos > 31:
-            return (0, 0, 0)
-        if pos < 10:
-            return (31 - pos * 3, pos * 3, 0)
-        if pos < 15:
-            pos -= 10
-            return (0, 31 - pos * 3,pos * 3)
-        pos -= 15
-        return (pos * 3, 0, 31 - pos * 3)
-     
-     
-    def rainbow_cycle(self, wait):
-        for j in range(256):
-            for i in range(self.get_number_of_leds()):
-                rc_index = (i * 256 // self.get_number_of_leds()) + j
-                self.pixels_set(i, self.wheel(rc_index & 31))
-            self.pixels_show()
-            time.sleep(wait)
--- pico_led/led.py
+++ pico_led/led.py
@@ -6,7 +6,7 @@ from NeoPixel import NeoPixel
 
 LED_ROW_COUNT = 16
 LED_COL_COUNT = 10
-TOTAL_LED_COUNT = LED_ROW_COUNT * LED_ROW_COUNT
+TOTAL_LED_COUNT = LED_ROW_COUNT * LED_COL_COUNT
 
 def turn_off_all_leds():
     strip.pixels_fill(colour.BLACK)
@@ -18,24 +18,46 @@ def show_led_line_by_line(pixel_position):
     if pixel_position_at_end_of_row:
             strip.pixels_show()
             time.sleep(0.05)
+            
+def wheel(pos):
+    # Input a value 0 to 31 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 31:
+        return (0, 0, 0)
+    if pos < 10:
+        return (31 - pos * 3, pos * 3, 0)
+    if pos < 15:
+        pos -= 10
+        return (0, 31 - pos * 3,pos * 3)
+    pos -= 15
+    return (pos * 3, 0, 31 - pos * 3)
+     
+     
+def rainbow_cycle(wait):
+    for j in range(256):
+        for i in range(TOTAL_LED_COUNT):
+            rc_index = (i * 256 // TOTAL_LED_COUNT) + j
+            strip.pixels_set(wheel(rc_index & 31), i)
+        strip.pixels_show()
+        time.sleep(wait)
        
 def pattern1():
     print('pattern1')
     for color in range(0,160):            
-        strip.color_chase(colour.COLORS[color%2], color)
+        strip.pixels_set(colour.COLORS[color%2], color)
         show_led_line_by_line(color)
     
     
 def pattern2():
     print('pattern2')
     for color in range(159,-1,-1):             
-        strip.color_chase(colour.COLORS[color%3], color)
+        strip.pixels_set(colour.COLORS[color%3], color)
         show_led_line_by_line(color)
     
 def pattern3():
     print('pattern3')
     for color in range(159,-1,-1):             
-        strip.color_chase(colour.COLORS[color%3], color)
+        strip.pixels_set(colour.COLORS[color%3], color)
         show_led_line_by_line(color)
     
 def pattern4():
@@ -44,7 +66,7 @@ def pattern4():
     num1 = 0
     num2 = 1
     for color in range(0,160):
-        strip.color_chase(colour.COLORS[color%4], color1)
+        strip.pixels_set(colour.COLORS[color%4], color1)
         color1 += 16
         if color1 > 159:
             num1 +=1
@@ -63,7 +85,7 @@ def pattern5():
     color2 = 15
     num3 = 15
     for color in range(0,160):
-        strip.color_chase(colour.COLORS[color%5], color2)
+        strip.pixels_set(colour.COLORS[color%5], color2)
         color2 += 16
         if color2 > 159:
             num3 -=1
@@ -80,7 +102,7 @@ def pattern6():
     num1 = 0
     num2 = 1       
     for color in range(0,160):
-        strip.color_chase(colour.BLACK, color1)
+        strip.pixels_set(colour.BLACK, color1)
         color1 += 16
         if color1 > 159:
             num1 +=1
@@ -96,14 +118,14 @@ def pattern6():
 def pattern7():
     print('pattern7')
     for color in range(0,160):
-        strip.color_chase(colour.PATTERN_121[color], color)
+        strip.pixels_set(colour.PATTERN_121[color], color)
     strip.pixels_show()
     time.sleep(0.5)
  
 def pattern_for_ever():
      print("pattern_for_ever")
      try:
-         while(1): strip.rainbow_cycle(0.002)
+         while(1): rainbow_cycle(0.002)
      except:
          turn_off_all_leds()
      
