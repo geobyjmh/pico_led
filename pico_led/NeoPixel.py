aa1ee96977c555c06352add1046c4c7af9720b23
geobyjmh <38050735+geobyjmh@users.noreply.github.com>
2022-02-18 10:21:27 +0000

    general tidy of file

--- pico_led/NeoPixel.py
+++ pico_led/NeoPixel.py
@@ -6,33 +6,35 @@ import ws2812_pio_driver
 class NeoPixel(object):
     def __init__(self,brightness=0.8):
         self.brightness = brightness
-        
         ws2812_pio_driver.create_and_run_pio_statemachine()
-
-        # Display a pattern on the LEDs via an array of LED RGB values.
-        self.ar = array.array("I", [0 for _ in range(self.get_number_of_leds())])
+        self.create_led_array()
           
-    ##########################################################################
+    def create_led_array(self):
+        self.leds = array.array("I", [0 for _ in range(self.get_number_of_leds())])
+          
     def get_number_of_leds(self):
         return ws2812_pio_driver.get_number_of_leds()
     
-    def __rgb(self, c):
-        r = int(((c >> 8) & 0xFF) * self.brightness)
-        g = int(((c >> 16) & 0xFF) * self.brightness)
-        b = int((c & 0xFF) * self.brightness)
-        combined_rgb = (g<<16) + (r<<8) + b
-        return combined_rgb
+    def __rgb_brightness(self, c, brightness):
+        r = int(((c >> 8) & 0xFF) * brightness)
+        g = int(((c >> 16) & 0xFF) * brightness)
+        b = int((c & 0xFF) * brightness)
+        adjusted_rgb = (g<<16) + (r<<8) + b
+        return adjusted_rgb
+    
+    def __rgb_combine(self, color):
+        return (color[1]<<16) + (color[0]<<8) + color[2]
         
     def pixels_show(self):
-        dimmer_ar = array.array("I", [0 for _ in range(self.get_number_of_leds())])
-        for i,c in enumerate(self.ar):
-            dimmer_ar[i] = self.__rgb(c)
-        ws2812_pio_driver.write_to_neopixel(dimmer_ar)
+        dimmer_leds = array.array("I", [0 for _ in range(self.get_number_of_leds())])
+        for i,c in enumerate(self.leds):
+            dimmer_leds[i] = self.__rgb_brightness(c, self.brightness)
+        ws2812_pio_driver.write_to_neopixel(dimmer_leds)
 
     def pixels_set(self,color, i):
-        self.ar[i] = (color[1]<<16) + (color[0]<<8) + color[2]
+        self.leds[i] = self.__rgb_combine(color)
 
     def pixels_fill(self, color):
-        for i in range(len(self.ar)):
+        for i in range(len(self.leds)):
             self.pixels_set(color, i)
      
