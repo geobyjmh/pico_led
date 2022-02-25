e547a98b7ab4c11553fb65924e3a394642441be5
geobyjmh <38050735+geobyjmh@users.noreply.github.com>
2022-02-25 16:48:39 +0000

    Add files via upload

--- bounce/led.py
+++ bounce/led.py
@@ -2,7 +2,6 @@
 import array, time
 import ws2812_pio_driver
 import colour
-import random
 from NeoPixel import NeoPixel
 from position import position
 
@@ -10,19 +9,33 @@ LED_ROW_COUNT = 16
 LED_COL_COUNT = 10
 TOTAL_LED_COUNT = LED_ROW_COUNT * LED_COL_COUNT
 
+LED_COUNT = 5
+
 def turn_off_all_leds():
     strip.pixels_fill(colour.BLACK)
     strip.pixels_show()
     time.sleep(0.05)
     
-  
-def pattern1():
-    print('pattern1')
+def init_position():
     x = position(0, LED_ROW_COUNT-1)
     y = position(0, LED_COL_COUNT-1)
-  
+    return x, y
+
+def add_pixel_to_list(pixels, x, y):
+    pixels.append([x.get_value(), y.get_value()])
+    if len(pixels) > LED_COUNT:
+        pixels.pop(0) 
+    return pixels
+
+def pattern1():
+    print('pattern1')
+    x,y = init_position()
+    pixels = list()
+
     while(1):
-        strip.pixel_set_xy(colour.WHITE, x.get_value(), y.get_value())
+        pixels = add_pixel_to_list(pixels, x, y)
+        for p in pixels:
+            strip.pixel_set_xy(colour.WHITE, p[0], p[1])
         strip.pixels_show()
         time.sleep(0.1)
         turn_off_all_leds()    
